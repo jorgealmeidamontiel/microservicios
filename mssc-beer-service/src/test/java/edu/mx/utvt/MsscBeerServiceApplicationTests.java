04b65c33f5c6f0d5e7ebdc40fc48fb8d6a203067
Jorge <Jorge@DESKTOP-048J275>
2023-06-17 11:45:51 -0600

    Initial commit

---
+++ mssc-beer-service/src/test/java/edu/mx/utvt/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mx.utvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
